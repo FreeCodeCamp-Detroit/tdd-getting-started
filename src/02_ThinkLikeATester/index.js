8c2b3d1db1e7af4beae39bdbf404dd5b426d5f0f
Danya D. Smith <danyadsmith@gmail.com>
2018-02-09 01:38:44 -0500

    LESSON THREE: Making it Work, Step 04 SOLUTION
    Here we write the code necessary to make our next test pass.

--- src/02_ThinkLikeATester/index.js
+++ src/02_ThinkLikeATester/index.js
@@ -1,6 +1,10 @@
 var getLongestWord = function (str) {
   if (typeof str === 'string') {
-    return str;
+    var words = str.replace(/[^a-zA-Z]/g, ' ').toLowerCase().split(' ');
+    var wordLengths = words.map((word) => word.length);
+    var longestLen = Math.max.apply(null, wordLengths);
+    var indexOfLongestWord = wordLengths.indexOf(longestLen);
+    return words[indexOfLongestWord];
   } else {
     const errStrType = new TypeError('The parameter str must be a string.');
     throw errStrType;
