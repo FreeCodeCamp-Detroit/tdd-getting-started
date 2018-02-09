13f6fb5b2996ffbdbdfe83274a95e6bd4c2954a3
Danya D. Smith <danyadsmith@gmail.com>
2018-02-09 01:01:35 -0500

    LESSON THREE: Making it Work, Step 03 SOLUTION
    Here we write the code necessary to make our next test pass.

--- src/02_ThinkLikeATester/index.js
+++ src/02_ThinkLikeATester/index.js
@@ -1,5 +1,10 @@
 var getLongestWord = function (str) {
-  return str;
+  if (typeof str === 'string') {
+    return str;
+  } else {
+    const errStrType = new TypeError('The parameter str must be a string.');
+    throw errStrType;
+  }
 };
 
 module.exports = getLongestWord;
