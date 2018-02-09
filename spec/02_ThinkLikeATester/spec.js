cb1d18dac61c9492ae33e32718582007e427bbd6
Danya D. Smith <danyadsmith@gmail.com>
2018-02-09 00:01:14 -0500

    LESSON TWO: Think Like a Tester SOLUTION, PART II
    In the second step of our solution, we describe the expectations of our
    tests. The spec in this example is complete, but the expectations are
    commented out. We will walk through a test-by-test example of
    red-green-refactor by removing the comment from one test at a time,
    making that test pass, then moving on to the next.

--- spec/02_ThinkLikeATester/spec.js
+++ spec/02_ThinkLikeATester/spec.js
@@ -6,10 +6,42 @@ const getLongestWord = require('../../src/02_ThinkLikeATester/');
 describe('[LESSON TWO: Think Like a Tester]', () => {
   describe('#getLongestWord(str)', () => {
     it('should be a function', () => {
-      expect(getLongestWord).to.be.a('function');
+      //expect(getLongestWord).to.be.a('function');
     });
     it('should return a string', () => {
-      expect(getLongestWord('What is the longest word in this sentence')).to.be.a('string');
+      //expect(getLongestWord('What is the longest word in this sentence')).to.be.a('string');
+    });
+    it('should throw an error when the first parameter is not a string', () => {
+      /*expect(() => {
+        getLongestWord(true);
+      }).to.throw(TypeError, 'The parameter str must be a string.');
+      expect(() => {
+        getLongestWord(2,);
+      }).to.throw(TypeError, 'The parameter str must be a string.');
+      expect(() => {
+        getLongestWord({});
+      }).to.throw(TypeError, 'The parameter str must be a string.');
+      expect(() => {
+        getLongestWord(null);
+      }).to.throw(TypeError, 'The parameter str must be a string.');
+      expect(() => {
+        getLongestWord(undefined);
+      }).to.throw(TypeError, 'The parameter str must be a string.');*/
+    });
+    it('should return the longest word converted to lowercase', () => {
+      //expect(getLongestWord('Juxtapose the strings')).to.eql('juxtapose');
+    });
+    it('should not remove apostrophes from the longest word', () => {
+      //expect(getLongestWord('They’re gone')).to.eql('they’re');
+    });
+    it('should return an empty string when passed an empty string', () => {
+      //expect(getLongestWord('')).to.eql('');
+    });
+    it('should remove trailing punctuation from the longest word', () => {
+      //expect(getLongestWord('The longest word I know is antidisestablishmentarianism.')).to.eql('antidisestablishmentarianism');
+    });
+    it('should return the first occurring word of all longest words', () => {
+      //expect(getLongestWord('hello world')).to.eql('hello');
     });
   });
 });
